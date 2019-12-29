0ec2838f64652961c4506fb5fc03468f2b70bfc7
Ashkan Farhangi <enterthebeginning@gmail.com>
2019-12-29 09:32:54 -0500

    A

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# time-series-decompostion
+time-series-decompostion
+========================
+
 Under the hood of time series.
+
+Sources:
+<https://www.kaggle.com/jdarcy/introducing-ssa-for-time-series-decomposition>
+
+Problem: Given a time-series signal, can you pinpoint the impact of each
+variable?  
+Lets say for a sales data there are about 10 factors
---
+++ outline.py
@@ -0,0 +1,30 @@
+#Outline 
+
+
+# Import
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# Input time series and preprocess
+df = read_csv('')
+
+# Plotting time series
+
+# Decomposition with SSA
+
+"""
+# of samples: 227
+window: 227/2  = 110
+Heatmap
+smaller heatmap
+
+"""
+
+# Extracting specific features like impact of hurricane
+
+
+
+# Output(Figures)
+
